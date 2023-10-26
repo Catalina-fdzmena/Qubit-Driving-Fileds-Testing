42faa02e97900737f21112f2825120a8295a2759
Andrea Catalina Fernández Mena <katy.fernandezmena@gmail.com>
2023-10-26 13:04:05 -0600

    final simulation from paper

--- tests_DrivingFields/driving_Fields.py
+++ tests_DrivingFields/driving_Fields.py
@@ -5,48 +5,71 @@ Made on: 20/10/2023 by: Andrea Catalina Fernandez Mena
 """
 # Import libraries
 import numpy as np
+import math
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 # Parameters
-g = 5
-delta = 4
-kappa = 0.6
-Omega1 = 10  #Example ∆
-t_max = 10    #max time por integration
+g = math.sqrt(5)  #periodicity condition
+delta = 4 #Delta imaginario δ
+kappa = 0.6   #Tests parameters 
+Omega1 = 5  
+t_max = 10  #Tests parameters 
 
 # Calculation function of η(t)
 def eta(t_values):
-    dt = 0.001  # Paso de integración
+    dt = 0.01
     integral = np.zeros_like(t_values)
-    
+
     for i, t in enumerate(t_values):
         s_values = np.linspace(0, t, int(t / dt))
         integrand = 1 / (np.cos(Omega1 * s_values) ** 2 + kappa ** 2 * np.sin(Omega1 * s_values) ** 2)
         integral[i] = np.trapz(integrand, dx=dt)
-    
+
     return integral
 
 # R(t) function
 def R(t_values):
     eta_t = eta(t_values)
-    return -1j * g * np.exp(1j * delta * eta_t) / ((np.cos(Omega1 * t_values) ** 2) + kappa ** 2 * np.sin(Omega1 * t_values) ** 2)
+    return (-1j * g * np.exp(1j * delta * eta_t)) / ((np.cos(Omega1 * t_values) ** 2) + kappa ** 2 * np.sin(Omega1 * t_values) ** 2)
 
 # Time values
 t_values = np.linspace(0, t_max, 1000)
 
-# Define real and imaginary values
+# Calculate ReR(t) and ImR(t)
 ReR = np.real(R(t_values))
 ImR = np.imag(R(t_values))
 
-# Graphics
-plt.figure(figsize=(10, 6))
-plt.plot(t_values, ReR, 'x', label='ReR(t)')
-plt.plot(t_values, ImR, 'y', label='ImR(t)')
-plt.xlabel('Tiempo')
-plt.ylabel('ReR(t) and ImR(t)')
-plt.legend()
-plt.grid(True)
-plt.title('Comportamiento de ReR(t) e ImR(t)')
-plt.show()
+# 3D Plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-#Modify paramtric graphic for be on time disposition
+# Plot ReR(t) and ImR(t)
+ax.plot(t_values, ReR, ImR, color='red', label='ReR and ImR')
+
+# Circles
+if kappa < 1:
+    # Blue circle of radius |g|
+    theta = np.linspace(0, 2 * np.pi, 100)
+    radius = np.abs(g)
+    x = radius * np.cos(theta)
+    y = radius * np.sin(theta)
+    z = np.zeros_like(x)
+    ax.plot(z, y, x, color='blue', alpha=0.5)
+
+    #Gray circle of radius |g|/κ^2
+    radius = np.abs(g) / (kappa ** 2)
+    x = radius * np.cos(theta)
+    y = radius * np.sin(theta)
+    ax.plot(z, y, x,  color='gray', alpha=0.5)
+
+ax.set_xlabel('Time')
+ax.set_ylabel('Re R(t)')
+ax.set_zlabel('Im R(t)')
+plt.title('3D Plot of Re R(t) and Im R(t) vs. Time')
+
+plt.show()
